b72e69cecba5cb39f94d9aeed45c5ff49ae8a73b
Shreya549 <shreya.chatterjee2018@vitstudent.ac.in>
2019-11-04 22:29:20 +0530

    Sign up enabled

---
+++ createLogin.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("BeHerChange");
+  dbo.createCollection("Credentials", function(err, res) {
+    if (err) throw err;
+    console.log("Collection created!");
+    db.close();
+  });
+});
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express()
 var bodyParser = require('body-parser');
 
+const mongoose = require('mongoose'); 
+mongoose.connect('mongodb://localhost:27017/BeHerChange'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+  
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
@@ -69,6 +77,45 @@ app.get('/view_stories', function(req, res){
 
     res.sendFile(__dirname + '/public/8_1)ViewStories.html');
 })
+
+app.get('/volunteer', function(req, res){
+    res.sendFile(__dirname + '/public/2_1)Signpage(volunteer).html');
+})
+
+app.get('/signin', function(req, res){
+    res.sendFile(__dirname + '/public/3_1)signin.html');
+})
+
+app.get('/signup', function(req, res){
+    res.sendFile(__dirname + '/public/4_1)signup.html');
+})
+
+app.post('/sign_up', function(req,res){ 
+    var name = req.body.fullname; 
+    var email =req.body.emailid; 
+    var gender = req.body.gender;
+    var pass = req.body.password; 
+    var phone =req.body.phonenumber; 
+    var dob = req.body.dob;
+    var username = req.body.username;
+  
+    var data = { 
+        "name": name, 
+        "email":email, 
+        "gender" : gender,
+        "password":pass, 
+        "phone":phone,
+        "dob" : dob,
+        "username" : username
+    } 
+    db.collection('Credentials').insertOne(data,function(err, collection){ 
+            if (err) throw err; 
+            console.log("Record inserted Successfully"); 
+                
+        }); 
+          
+    return res.redirect(__dirname + '/public/3_1)signin.html'); 
+}) 
 
 app.listen (8000, function(){
     console.log("Server running on port : 8000");
